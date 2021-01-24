253112b17ed581cb05f4a738ff8b3dc7c84ee693
andre.frolov44@gmail.com <andre.frolov44@gmail.com>
2021-01-24 22:21:58 +0300

    create models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Brand, Product, ProductImage, ProductSpecification, CartItem, Cart
+
+
+class SpecificationInLine(admin.TabularInline):
+    model = ProductSpecification
+
+
+class ImageInLine(admin.TabularInline):
+    model = ProductImage
+
+
+class CartItemInLine(admin.TabularInline):
+    model = CartItem
+
+    def has_add_permission(self, request, obj):
+        return False
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'price', 'sale', 'priceSale', 'available', 'newProduct', 'topSale',)
+    list_editable = ('available', 'newProduct', 'topSale',)
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [SpecificationInLine, ImageInLine]
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'address', 'order_date', 'active',)
+    list_editable = ('active',)
+    inlines = [CartItemInLine]
+
+    def has_add_permission(self, request):
+        return False
--- store/models.py
+++ store/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = ("Categories")
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=0)
+    sale = models.PositiveIntegerField(blank=True, null=True)
+    priceSale = models.DecimalField(max_digits=6, decimal_places=0, blank=True, null=True)
+    image = models.ImageField(upload_to='product')
+    available = models.BooleanField(default=True)
+    newProduct = models.BooleanField(default=False)
+    topSale = models.BooleanField(default=False)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSpecification(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=400)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image
+
+
+class Cart(models.Model):
+    phone_number = models.CharField(max_length=12)
+    active = models.BooleanField(default=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.phone_number
+
+
+class CartItem(models.Model):
+    food = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_item")
+    order_date = models.DateTimeField(auto_now_add=True)
